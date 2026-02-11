1c3b63b0a55bf99ecd2883ca97442aa071066efc
mia005 <93291049+mia005@users.noreply.github.com>
2026-02-12 10:02:50 +1300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,13 +2,13 @@ import "./globals.css";
 
 export const metadata = {
   title: "Toomaga Payment System",
-  description: "Live Demo"
+  description: "Loan Management Dashboard",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, padding: 0 }}>
+      <body>
         {children}
       </body>
     </html>
