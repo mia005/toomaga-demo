f38cbbe8153e25e45ee87911cb75d45d041d85a2
mia005 <93291049+mia005@users.noreply.github.com>
2026-02-11 21:58:08 +1300

    Update LoanListTable.js

--- app/components/LoanListTable.js
+++ app/components/LoanListTable.js
@@ -1,40 +1,50 @@
 import { currency } from "../lib/format";
 
-export default function LoanListTable({ loans, churches, onSelect }) {
+export default function LoanListTable({ loans, churches }) {
   return (
-    <table style={tableStyle}>
-      <thead>
-        <tr>
-          <th>Loan Ref</th>
-          <th>Church</th>
-          <th>Principal</th>
-          <th>Balance</th>
-          <th>Interest</th>
-        </tr>
-      </thead>
-      <tbody>
-        {loans.map((loan) => {
-          const church = churches.find(
-            (c) => c.id === loan.church_id
-          );
+    <div style={wrapper}>
+      <h3>Loan Applications</h3>
 
-          return (
-            <tr key={loan.id} onClick={() => onSelect(loan)}>
-              <td>{loan.loan_ref}</td>
-              <td>{church?.church_name}</td>
-              <td>{currency(loan.principal)}</td>
-              <td>{currency(loan.balance)}</td>
-              <td>{loan.interest_rate}%</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+      <table style={tableStyle}>
+        <thead>
+          <tr>
+            <th>Loan Ref</th>
+            <th>Church</th>
+            <th>Principal</th>
+            <th>Balance</th>
+            <th>Interest Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+          {loans.map((loan) => {
+            const church = churches.find(
+              (c) => c.id === loan.church_id
+            );
+
+            return (
+              <tr key={loan.id}>
+                <td>{loan.loan_ref}</td>
+                <td>{church?.church_name}</td>
+                <td>{currency(loan.principal)}</td>
+                <td>{currency(loan.balance)}</td>
+                <td>{loan.interest_rate}%</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 }
+
+const wrapper = {
+  padding: "20px",
+  background: "white",
+  borderRadius: "12px",
+  boxShadow: "0 4px 10px rgba(0,0,0,0.05)",
+};
 
 const tableStyle = {
   width: "100%",
   borderCollapse: "collapse",
-  marginTop: "40px",
 };
