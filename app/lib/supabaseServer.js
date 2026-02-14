72ef171d9157c6133250366f98d0a9ad8e6ab6da
mia005 <93291049+mia005@users.noreply.github.com>
2026-02-14 13:25:57 +1300

    Update supabaseServer.js

--- app/lib/supabaseServer.js
+++ app/lib/supabaseServer.js
@@ -1,8 +1,18 @@
-import { createClient } from "@supabase/supabase-js";
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
 
-export function createServerClient() {
-  return createClient(
+export function createClient() {
+  const cookieStore = cookies();
+
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.SUPABASE_SERVICE_ROLE_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        get(name) {
+          return cookieStore.get(name)?.value;
+        },
+      },
+    }
   );
 }
